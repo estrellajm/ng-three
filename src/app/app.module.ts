f491d88ff64af6c64c928f0bc510622ea1fd63c4
Jose Estrella <estrellajosem@gmail.com>
2023-09-11 12:21:37 -0600

    added model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CubeComponent } from './cube/cube.component';
+import { ModelComponent } from './model/model.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CubeComponent
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CubeComponent, ModelComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
