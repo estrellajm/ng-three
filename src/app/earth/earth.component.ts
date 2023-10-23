fb4b008222ca6b77ae78237635df68c4ed55d045
Jose Estrella <estrellajosem@gmail.com>
2023-10-22 18:29:50 -0600

    loading the earth

--- src/app/earth/earth.component.ts
+++ src/app/earth/earth.component.ts
@@ -1,10 +1,161 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
 
 @Component({
   selector: 'app-earth',
   templateUrl: './earth.component.html',
-  styleUrls: ['./earth.component.scss']
+  styleUrls: ['./earth.component.scss'],
 })
 export class EarthComponent {
+  @ViewChild('canvas') private canvasRef!: ElementRef;
 
+  //* Stage Properties
+
+  @Input() public fieldOfView: number = 1;
+
+  @Input('nearClipping') public nearClippingPane: number = 1;
+
+  @Input('farClipping') public farClippingPane: number = 1000;
+
+  //? Scene properties
+  private camera!: THREE.PerspectiveCamera;
+
+  private controls!: OrbitControls;
+
+  private ambientLight!: THREE.AmbientLight;
+
+  private light1!: THREE.PointLight;
+
+  private light2!: THREE.PointLight;
+
+  private light3!: THREE.PointLight;
+
+  private light4!: THREE.PointLight;
+
+  private model: any;
+
+  private directionalLight!: THREE.DirectionalLight;
+
+  // ? Helper Properties (Private Properties);
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private loaderGLTF = new GLTFLoader();
+
+  private renderer!: THREE.WebGLRenderer;
+
+  private scene!: THREE.Scene;
+
+  /**
+   *Animate the model
+   *
+   * @private
+   * @memberof ModelComponent
+   */
+  private animateModel() {
+    if (this.model) {
+      this.model.rotation.z += 0.5;
+    }
+  }
+
+  /**
+   *create controls
+   *
+   * @private
+   * @memberof ModelComponent
+   */
+  private createControls = () => {
+    const renderer = new CSS2DRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.style.position = 'absolute';
+    renderer.domElement.style.top = '0px';
+    document.body.appendChild(renderer.domElement);
+    this.controls = new OrbitControls(this.camera, renderer.domElement);
+    this.controls.autoRotate = true;
+    this.controls.enableZoom = true;
+    this.controls.enablePan = false;
+    this.controls.update();
+  };
+
+  /**
+   * Create the scene
+   *
+   * @private
+   * @memberof CubeComponent
+   */
+  private createScene() {
+    /** Scene */
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(
+      0x000000
+    ); /** background color (space) */
+
+    /** Camera */
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      innerWidth / innerHeight,
+      0.1,
+      1000
+      // this.fieldOfView,
+      // aspectRatio,
+      // this.nearClippingPane,
+      // this.farClippingPane
+    );
+
+    /** create sphere */
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(5, 50, 50),
+      new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load('assets/earth.jpeg'),
+      })
+    );
+
+    this.scene.add(sphere);
+
+    this.camera.position.z = 10;
+  }
+
+  private getAspectRatio() {
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  /**
+   * Start the rendering loop
+   *
+   * @private
+   * @memberof CubeComponent
+   */
+  private startRenderingLoop() {
+    //* Renderer
+    // Use canvas element in template
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    let component: EarthComponent = this;
+    (function render() {
+      component.renderer.render(component.scene, component.camera);
+      component.animateModel();
+      requestAnimationFrame(render);
+    })();
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.createScene();
+    this.startRenderingLoop();
+    this.createControls();
+  }
 }
